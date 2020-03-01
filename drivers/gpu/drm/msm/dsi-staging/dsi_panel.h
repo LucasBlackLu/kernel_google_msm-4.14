48b58f5792d8bef23e45d0f7e5c8049dfe05b3e7
tbalden <illespal@gmail.com>
2020-03-01 01:01:59 -0800

    hbm: uci: don't do Off part when screen turns off, and trylock
    - don't do hbm switching when screen turns off, it will be off by itself
    - don't block panel lock with uci hbm switching. if locked don't wait
    
    (cherry picked from commit c53a51b85d4794375abafffb63c734fd27943cde)
    (cherry picked from commit 2ea4ad1f9430ce70cf2bba9683b9f323259206db)
    (cherry picked from commit 40bbf69f80c18ce85dc3d145026de7e1715a1971)
    (cherry picked from commit 8bd314e2a77a70aeccd73d6299903d44d2780984)

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -532,6 +532,10 @@ int dsi_panel_get_vendor_extinfo(struct dsi_panel *panel);
 /* Set/get high brightness mode */
 int dsi_panel_update_hbm(struct dsi_panel *panel, enum hbm_mode_type);
 enum hbm_mode_type dsi_panel_get_hbm(struct dsi_panel *panel);
+#ifdef CONFIG_UCI
+// special try locking version...
+int dsi_panel_try_update_hbm(struct dsi_panel *panel, enum hbm_mode_type);
+#endif
 
 int dsi_panel_bl_update_irc(struct dsi_backlight_config *bl, bool enable);
 
