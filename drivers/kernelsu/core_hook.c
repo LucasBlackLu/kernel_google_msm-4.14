ae9773fa32b456b3d29f22933e4ab430d5430400
chiteroman <querveros999@gmail.com>
2024-12-21 00:02:46 +0100

    Disable KPROBES
    grep -rl 'CONFIG_KPROBES' kernel/ | xargs sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g; s/#ifndef CONFIG_KPROBES/#if 1/g'

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -848,7 +848,7 @@ void __init ksu_core_init(void)
 
 void ksu_core_exit(void)
 {
-#ifdef CONFIG_KPROBES
+#if 0
 	pr_info("ksu_core_kprobe_exit\n");
 	// we dont use this now
 	// ksu_kprobe_exit();
